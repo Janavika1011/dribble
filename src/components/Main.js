755d7ea1456a8cb8e464c415116c1d3c51e85502
Janavika <101445874+Janavika1011@users.noreply.github.com>
2023-12-15 16:40:43 +0530

    Add files via upload

---
+++ src/components/Main.js
@@ -0,0 +1,21 @@
+// Main.js
+
+import React from 'react';
+import './Main.css'; // Import the CSS file
+
+function Main() {
+  return (
+    <div className="main-container">
+      <div className="main-content">
+        <h2>Discover the World's top designers & Creatives</h2>
+        <p>Dribbble is the leading destination to find & showcase creative work and home to the world's best design professionals</p>
+        <button>Sign up</button>
+      </div>
+      <div className="main-image">
+        <img src="https://cdn.dribbble.com/userupload/11918547/file/original-7531aebaa337ed7eeb4cc333f37230bc.jpg?resize=400x300&vertical=center" alt="" />
+      </div>
+    </div>
+  );
+}
+
+export default Main;
